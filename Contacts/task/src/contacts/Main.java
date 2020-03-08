f16d8676f738a47bb0d2bba5720bf6ec2f48da3c
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-03-08 12:12:10 +0100

    refactored

--- Contacts/task/src/contacts/Main.java
+++ Contacts/task/src/contacts/Main.java
@@ -1,7 +1,25 @@
 package contacts;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the name of the person:");
+        String name = scanner.nextLine();
+        System.out.println(name);
+
+        System.out.println("Enter the surname of the person:");
+        String surname = scanner.nextLine();
+        System.out.println(surname);
+
+        System.out.println("Enter the number:");
+        String number = scanner.nextLine();
+        System.out.println(number);
+
+        System.out.println("A record created!");
+        System.out.println("A Phone Book with a single record created!");
     }
+
 }
