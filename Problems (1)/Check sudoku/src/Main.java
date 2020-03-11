cfd526ce8f789affa7c4512e3ae2f526aa6191ff
jarpar <partyka.jaroslaw@gmail.com>
2020-03-11 12:06:17 +0100

    first attempts to solve sudoku

--- Problems (1)/Check sudoku/src/Main.java
+++ Problems (1)/Check sudoku/src/Main.java
@@ -8,6 +8,17 @@ public class Main {
     }
 }
 
+class SmallTabs extends Tab {
+
+    public int[] divide() {
+        for (int i = 0; i < n * n; i++) {
+
+        }
+        return;
+    }
+
+}
+
 class Tab {
     int n;
     int[][] matrix;
