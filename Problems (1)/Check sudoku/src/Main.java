589c19c1d4439dbc54a1a9e7b314e42719839546
jarpar <partyka.jaroslaw@gmail.com>
2020-03-11 11:50:28 +0100

    problem of the day solved!

---
+++ Problems (1)/Check sudoku/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Tab tab = new Tab();
+        tab.read();
+        tab.print();
+    }
+}
+
+class Tab {
+    int n;
+    int[][] matrix;
+
+    public int[][] read() {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        matrix = new int[n * n][n * n];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        scanner.close();
+        return matrix;
+    }
+
+    public void print() {
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println();
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+        }
+    }
+}
